163bd89945d2ab38dadd66eaef62429879e9bf2d
shivamm070402 <shivammohite2020@gmail.com>
2025-04-30 11:25:55 +0530

    publish this code on 30th April

--- src/logger.py
+++ src/logger.py
@@ -1,24 +1,19 @@
-import os
 import logging
+import os
 from datetime import datetime
-import sys
 
-from src.exception import CustomException
+LOG_FILE=f"{datetime.now().strftime('%m_%d_%Y_%H_%M_%S')}.log"
+logs_path=os.path.join(os.getcwd(),"logs",LOG_FILE)
+os.makedirs(logs_path,exist_ok=True)
 
-# Step 1: Create logs directory if it doesn't exist
-LOG_DIR = os.path.join(os.getcwd(), "logs")
-os.makedirs(LOG_DIR, exist_ok=True)
+LOG_FILE_PATH=os.path.join(logs_path,LOG_FILE)
 
-# Step 2: Generate log file name with timestamp
-LOG_FILE = f"{datetime.now().strftime('%m_%d_%Y_%H_%M_%S')}.log"
-LOG_FILE_PATH = os.path.join(LOG_DIR, LOG_FILE)
-
-# Step 3: Configure logging
 logging.basicConfig(
     filename=LOG_FILE_PATH,
-    format="[%(asctime)s] [Line: %(lineno)d] [%(name)s] [%(levelname)s] - %(message)s",
+    format="[ %(asctime)s ] %(lineno)d %(name)s - %(levelname)s - %(message)s",
     level=logging.INFO,
-)
 
+
+)
 
 
